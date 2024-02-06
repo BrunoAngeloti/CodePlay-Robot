6303125891ec4ebd953e08639ef7d0fd73dfe5fb
BrunoAngeloti <bruno.angeloti@terra.com.br>
2024-02-05 21:02:03 -0300

    feat: atualizações

--- app/App.js
+++ app/App.js
@@ -1,10 +1,17 @@
 // App.js
-import React, { useState } from 'react';
-import { SafeAreaView, Button, Modal, TouchableOpacity, Text, View } from 'react-native';
-import DraggableBlocks from './components/DraggableBlocks';
-import { blocksToArray } from './helpers/helperFunctions';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import socket from './services/socketio';
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  Button,
+  Modal,
+  TouchableOpacity,
+  Text,
+  View,
+} from "react-native";
+import DraggableBlocks from "./components/DraggableBlocks";
+import { blocksToArray } from "./helpers/helperFunctions";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import socket from "./services/socketio";
 
 const App = () => {
   const [showCatalog, setShowCatalog] = useState(false);
@@ -18,21 +25,23 @@ const App = () => {
   };
 
   const handleAddBlock = (block) => {
-    setBlocks((prevBlocks) => [...prevBlocks, { ...block, key: `${block.key}_${blockCounter}` }]);
+    setBlocks((prevBlocks) => [
+      ...prevBlocks,
+      { ...block, key: `${block.key}_${blockCounter}` },
+    ]);
     setBlockCounter((prevCounter) => prevCounter + 1);
     setShowCatalog(false);
   };
 
   const testeEmit = () => {
     if (statusLed === 0) {
-      socket.emit('data', 1);
+      socket.emit("data", 1);
       setStatusLed(1);
     } else {
-      socket.emit('data', 0);
+      socket.emit("data", 0);
       setStatusLed(0);
     }
   };
-
 
   const teste = (blocks) => {
     console.log(blocks);
@@ -61,35 +70,78 @@ const App = () => {
       <Button title="Adicionar bloco" onPress={() => setShowCatalog(true)} />
       <Button title="Executa" onPress={() => teste(blocks)} />
 
-      {/*<Button 
-        title={statusLed === 0 ? 'Ligar LED' : 'Desligar LED'}
-        onPress={() => testeEmit()} 
-      />*/}
+      <Button
+        title={statusLed === 0 ? "Ligar LED" : "Desligar LED"}
+        onPress={() => testeEmit()}
+      />
 
-      <GestureHandlerRootView style={{flex: 1}}>
-        <DraggableBlocks blocks={blocks} onArrayGenerated={handleArrayGenerated} />
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <DraggableBlocks
+          blocks={blocks}
+          onArrayGenerated={handleArrayGenerated}
+        />
       </GestureHandlerRootView>
       <Modal visible={showCatalog} onRequestClose={() => setShowCatalog(false)}>
         <View style={{ padding: 20 }}>
           <TouchableOpacity
-            onPress={() => handleAddBlock({ id: 'move_forward', key: 'move_forward', name: 'Mover para frente', data: { steps: 1 } })}
-            style={{ backgroundColor: 'lightblue', padding: 10, borderRadius: 5, marginBottom: 10 }}
+            onPress={() =>
+              handleAddBlock({
+                id: "move_forward",
+                key: "move_forward",
+                name: "Mover para frente",
+                data: { steps: 1 },
+              })
+            }
+            style={{
+              backgroundColor: "lightblue",
+              padding: 10,
+              borderRadius: 5,
+              marginBottom: 10,
+            }}
           >
             <Text>Mover para frente</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            onPress={() => handleAddBlock({ id: 'move_back', key: 'move_back', name: 'Mover para tras', data: { steps: 1 } })}
-            style={{ backgroundColor: 'lightblue', padding: 10, borderRadius: 5, marginBottom: 10 }}
+            onPress={() =>
+              handleAddBlock({
+                id: "move_back",
+                key: "move_back",
+                name: "Mover para tras",
+                data: { steps: 1 },
+              })
+            }
+            style={{
+              backgroundColor: "lightblue",
+              padding: 10,
+              borderRadius: 5,
+              marginBottom: 10,
+            }}
           >
             <Text>Mover para tras</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            onPress={() => handleAddBlock({ id: 'if_block', key: 'if_block', name: 'Se', data: { condition: 'true', childrenTrue: [], childrenFalse: [] } })}
-            style={{ backgroundColor: 'lightblue', padding: 10, borderRadius: 5, marginBottom: 10 }}
+            onPress={() =>
+              handleAddBlock({
+                id: "if_block",
+                key: "if_block",
+                name: "Se",
+                data: {
+                  condition: "true",
+                  childrenTrue: [],
+                  childrenFalse: [],
+                },
+              })
+            }
+            style={{
+              backgroundColor: "lightblue",
+              padding: 10,
+              borderRadius: 5,
+              marginBottom: 10,
+            }}
           >
             <Text>Se</Text>
           </TouchableOpacity>
-          {/* Adicione outros blocos ao catálogo aqui.*/ }
+          {/* Adicione outros blocos ao catálogo aqui.*/}
         </View>
       </Modal>
     </SafeAreaView>
